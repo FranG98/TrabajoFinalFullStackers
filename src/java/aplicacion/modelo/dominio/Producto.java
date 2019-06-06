2efb52584f4e7b12520c28cca02f87b4c810b11c
Cruz Ruth <jack@DESKTOP-BN9P691>
2019-06-06 12:19:10 -0300

    modificacion de la clase Producto, agregue mas atributos junto a sus metodos accesores

--- src/java/aplicacion/modelo/dominio/Producto.java
+++ src/java/aplicacion/modelo/dominio/Producto.java
@@ -17,38 +17,63 @@ public class Producto implements Serializable{
     //codigo: representa el codigo unico de cada producto
     private String nombreProducto;
     //nombreProducto: representa el producto
-    private String rubro;
-    //rubro: nombre que representa un conjunto de productos que comparten las mismas caracteristicas
-    private float precio;
+    private String Sector;
+    //sector: nombre que representa un conjunto de productos que comparten las mismas caracteristicas
+    private Float precio;
     //precio: representa el precio de venta del producto al publico
-    private short stock;
+    private Short stock;
     //stock: representa la cantidad dispronible del producto 
+    private String material;
+    //material: representa material del producto
+    private String marca;
+    //marca: representa la marca del producto
+    private String origen;
+    //origen: representa el lugar de origen del producto
+    private Boolean prodOferta;
+    //prodOferta: representa si un producto esta en oferta o no 
+    private String dimension;
+    //dimencion: representa las medidas del producto 
+    private String imagen;
+    //imagen: representa el nombre de la imagen
     
     //CONSTRUCTORES
     /**
      * constructor sin parametros
      */
     public Producto() {
+        
     }
-    
-    
     /**
      * constructor parametrizado
-     * @param codigo variable que almacena el codigo del producto
-     * @param nombreProducto variable que almacena en nombre del producto
-     * @param rubro variable que almacena el rubro del producto
-     * @param precio variable que almacena el precio del producto
-     * @param stock valriable que almacena el stock del producto
+     * @param codigo almacena el código del Producto
+     * @param nombreProducto almacena el nombre del Producto
+     * @param Sector almacena el sector del Producto
+     * @param precio almacena el precio del Producto
+     * @param stock almacena el stock del Producto
+     * @param material almacena el material del Producto
+     * @param marca almacena la marca del Producto
+     * @param origen almacena el origen del Producto
+     * @param prodOferta almacena si el producto esta en oferta, prodOferta inicializa en falso 
+     * @param dimension almacena la dimencion del Producto
+     * @param imagen almacena nombre de la imagen del Producto 
      */
-    public Producto(String codigo, String nombreProducto, String rubro, float precio, short stock) {
+    
+    public Producto(String codigo, String nombreProducto, String Sector, Float precio, Short stock, String material, String marca, String origen, Boolean prodOferta, String dimension, String imagen) {
         this.codigo = codigo;
         this.nombreProducto = nombreProducto;
-        this.rubro = rubro;
+        this.Sector = Sector;
         this.precio = precio;
         this.stock = stock;
+        this.material = material;
+        this.marca = marca;
+        this.origen = origen;
+        prodOferta = false;
+        this.dimension = dimension;
+        this.imagen = imagen;
     }
     
     //GETTER AND SETTER
+     
     /**
      * @return the codigo
      */
@@ -78,50 +103,137 @@ public class Producto implements Serializable{
     }
 
     /**
-     * @return the rubro
+     * @return the Sector
      */
-    public String getRubro() {
-        return rubro;
+    public String getSector() {
+        return Sector;
     }
 
     /**
-     * @param rubro the rubro to set
+     * @param Sector the Sector to set
      */
-    public void setRubro(String rubro) {
-        this.rubro = rubro;
+    public void setSector(String Sector) {
+        this.Sector = Sector;
     }
 
     /**
      * @return the precio
      */
-    public float getPrecio() {
+    public Float getPrecio() {
         return precio;
     }
 
     /**
      * @param precio the precio to set
      */
-    public void setPrecio(float precio) {
+    public void setPrecio(Float precio) {
         this.precio = precio;
     }
 
     /**
      * @return the stock
      */
-    public short getStock() {
+    public Short getStock() {
         return stock;
     }
 
     /**
      * @param stock the stock to set
      */
-    public void setStock(short stock) {
+    public void setStock(Short stock) {
         this.stock = stock;
     }
 
-    @Override
-    public String toString() {
-        return "Codigo del Producto: "+codigo+"Nombre del producto: "+nombreProducto+"Precio del Producto: "+precio+"Stock del Producto: "+stock+"Rubro del Producto: "+rubro;
+    /**
+     * @return the material
+     */
+    public String getMaterial() {
+        return material;
+    }
+
+    /**
+     * @param material the material to set
+     */
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @return the origen
+     */
+    public String getOrigen() {
+        return origen;
+    }
+
+    /**
+     * @param origen the origen to set
+     */
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    /**
+     * @return the prodOferta
+     */
+    public Boolean getProdOferta() {
+        return prodOferta;
+    }
+
+    /**
+     * @param prodOferta the prodOferta to set
+     */
+    public void setProdOferta(Boolean prodOferta) {
+        this.prodOferta = prodOferta;
+    }
+
+    /**
+     * @return the dimension
+     */
+    public String getDimension() {
+        return dimension;
+    }
+
+    /**
+     * @param dimension the dimencion to set
+     */
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+
+    /**
+     * @return the imagen
+     */
+    public String getImagen() {
+        return imagen;
+    }
+
+    /**
+     * @param imagen the imagen to set
+     */
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
     }
     
+    @Override
+    public String toString() {
+        return "Codigo del Producto: "+getCodigo()+"Nombre del producto: "+getNombreProducto()+"Precio del Producto: "+getPrecio()+
+               "Stock del Producto: "+getStock()+"Sector del Producto: "+getSector()+"Material del Producto "+getMaterial()+
+               "Origen del Producto: "+getOrigen()+"Dimension del Producto: "+getDimension()+"Marca del Producto: "+getMarca()+
+               "Producto en Oferta: "+getProdOferta()+"Direccion de Imagen: "+getImagen();
+    }
+
 }
