03639d87b29dacefced52e35815de27f4f582752
FranG98 <50504986+FranG98@users.noreply.github.com>
2019-06-06 22:43:59 -0300

    Se agrego el atributo usuarioComprador a la clase Venta Tambien se modifico el tipo de dato List por el Set para la futura implementacion de la base de datos

--- src/java/aplicacion/modelo/dominio/Venta.java
+++ src/java/aplicacion/modelo/dominio/Venta.java
@@ -7,7 +7,8 @@ package aplicacion.modelo.dominio;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * La clase venta contiene todos los atributos necesarios para registrar una venta realizada en la página
@@ -21,13 +22,15 @@ public class Venta {
     //Estado de la venta. Tomará 3 valores "Pendiente", "Realizada" o "Cancelada"
     private Calendar fechaHoraVenta;
     //La fecha y hora de la venta realizada
-    private List<Producto> productos;
+    private Set<Producto> productos;
     //Lista de productos que forman parte de una venta
     private Short ganancia;
     //Dinero que se recibe de la venta
     private Short codigoVenta;
     //Codigo que identifica a cada venta
-
+    private Usuario usuarioComprador;
+    //Usuario que solicito la compra de Productos
+    
     /**
      * Constructor sin parametros
      */
@@ -40,14 +43,15 @@ public class Venta {
      * @param fechaHoraVenta define la fecha y hora en la cual se realizo la venta; Se utiliza el tipo Calendar ya que trabaja con ambas
      * @param ganancia define el dinero que se recibe por la venta
      * @param codigoVenta define el codigo identificador de la venta; Más adelante se utilizará numeros aleatorios para esto
-     * @param productos define la lista de productos que se reservaron para la venta
+     * @param usuarioComprador define el usuario que esta realizando una com
      */
-    public Venta(String estadoVenta, Calendar fechaHoraVenta, List<Producto> productos, Short ganancia, Short codigoVenta) {
+    public Venta(String estadoVenta, Calendar fechaHoraVenta, Short ganancia, Short codigoVenta, Usuario usuarioComprador) {
         this.estadoVenta = estadoVenta;
         this.fechaHoraVenta = fechaHoraVenta;
-        this.productos = productos;
+        productos = new HashSet<>();
         this.ganancia = ganancia;
         this.codigoVenta = codigoVenta;
+        this.usuarioComprador = usuarioComprador;
     }
 
     //METODOS GETTER & SETTER
@@ -110,15 +114,29 @@ public class Venta {
         /**
      * @return the productos
      */
-    public List<Producto> getProductos() {
+    public Set<Producto> getProductos() {
         return productos;
     }
 
     /**
      * @param productos the productos to set
      */
-    public void setProductos(List<Producto> productos) {
+    public void setProductos(Set<Producto> productos) {
         this.productos = productos;
+    }
+    
+    /**
+     * @return the usuarioComprador
+     */
+    public Usuario getUsuarioComprador() {
+        return usuarioComprador;
+    }
+
+    /**
+     * @param usuarioComprador the usuarioComprador to set
+     */
+    public void setUsuarioComprador(Usuario usuarioComprador) {
+        this.usuarioComprador = usuarioComprador;
     }
     
     //Metodo toString
@@ -128,4 +146,5 @@ public class Venta {
         //Se utiliza el SimpleDateFormat para mostrar la fecha formalmente
         return "Codigo de Venta: "+codigoVenta+" // Estado de Venta: "+estadoVenta+" // Ganancia de Venta: "+ganancia+" // Fecha y Hora de la Venta: "+sdf.format(fechaHoraVenta.getTime());
     }
+
 }
